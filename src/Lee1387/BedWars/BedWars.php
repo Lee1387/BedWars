0fd55b7e2810885f43c288e79b829034ecebd338
Lee <leeadams5902@gmail.com>
2024-08-26 17:07:38 +0100

    first commit

---
+++ src/Lee1387/BedWars/BedWars.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lee1387\BedWars;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\SingletonTrait;
+
+class BedWars extends PluginBase {
+
+    use SingletonTrait;
+}
